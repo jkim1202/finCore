82338692d843183c3c6bdcd4b0d146e745cf644a
jkim1202 <junhyuk317@gmail.com>
2026-04-17 18:37:29 +0900

    chore: bootstrap project

---
+++ src/main/java/org/example/fincore/FinCoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.fincore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinCoreApplication.class, args);
+    }
+
+}
